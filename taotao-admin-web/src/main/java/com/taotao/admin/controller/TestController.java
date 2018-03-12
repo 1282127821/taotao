fca1b455bd2411a67372041c6e3f611014b17415
Administrator <1282127821@qq.com>
2018-03-12 15:13:42 +0800

    44

--- taotao-admin-web/src/main/java/com/taotao/admin/controller/TestController.java
+++ taotao-admin-web/src/main/java/com/taotao/admin/controller/TestController.java
@@ -1,5 +1,10 @@
 package com.taotao.admin.controller;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +14,8 @@ import com.taotao.admin.service.TestService;
 public class TestController {
 	
 	/** 注入服务接口代理对象 */
-	@Autowired
+	//@Autowired
+	@Resource(name="testService1")
 	private TestService testService;
 	
 	@GetMapping("/date")
